df2f8219a505f785b5fdadfd276f95996f7460be
Brayan Cardona <brayancardona3778@gmail.com>
2024-09-29 21:37:31 -0500

    add new card, called card2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Cards1 from './componentes/card1';
+import Card2 from './componentes/card2';
 
 
 function App() {
   return (<>
     <div className='contenedorPrincipal'>
       <Cards1 />
+      <Card2/>
 
     </div>
 
--- src/componentes/card2.jsx
+++ src/componentes/card2.jsx
@@ -1,9 +1,15 @@
-import React from "react";
-const Card2 = ({lang, img}) => {
+import imgFruitCard2 from './images/imgFruitCard2.jpg'
+
+
+const Card2 = () => {
     return (
-        <div className="card" style={{backgroundColor: "#cacaf8"}}>
-            <img src={img} alt="un paisaje" />
-            <h3>{lang}</h3>
+        <div className="relative max-w-[280px] h-[386px] transition-all hover:scale-[1.1] flex flex-col-reverse">
+            <img className='w-full h-full object-cover absolute' src={imgFruitCard2} alt="fruit-img" />
+            <div className='z-10 rounded-t-[10%] text-center bg-[#a5ca8e] gap-4 flex flex-col justify-center items-center p-5'>
+                <h2 className='font-bold text-2xl'>Make your <span className='text-[#39731d]'>choice</span> right now!</h2>
+                <p className='text-xs'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                {/* <a href='/'>{'Learn How ->'}</a> */}
+            </div>
 
         </div>)
 }
